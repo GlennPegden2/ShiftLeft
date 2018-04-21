43e33e632debf480a83277340a589b644d11584d
Glenn Pegden <glenn.pegden@skybettingandgaming.com>
2018-04-21 14:15:30 +0100

    Determine filename from plugin name

--- ShiftLeft.py
+++ ShiftLeft.py
@@ -1,4 +1,22 @@
-import urllib
-import httplib2
 
-print("Hello World")
+import urllib.request
+import sys
+import re
+
+def downloadWPPlugin (pluginName):
+    if not re.match("^[a-z_-]*$", pluginName):
+        print("Error! invalid chars in plugin name")
+        sys.exit()
+
+    pluginpage = "https://wordpress.org/plugins/" + pluginName + "/"
+    response = urllib.request.urlopen(pluginpage)
+    html = response.read()
+    htmlstr = html.decode("utf-8")
+#    url=re.search("\"d(own)loadUrl\":\\s\"(.+?)\",",htmlstr,re.IGNORECASE)
+    url=re.search('"downloadUrl":\\s"(.+?)",',htmlstr,re.IGNORECASE)
+    print(url.group(1))
+
+    return
+
+html = downloadWPPlugin("wordpress-seo")
+print(html)
