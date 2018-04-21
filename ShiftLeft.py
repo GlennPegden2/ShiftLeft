035ee252651fdf9be32884b4be30c93c5602076d
Glenn Pegden <glenn.pegden@skybettingandgaming.com>
2018-04-21 14:43:44 +0100

    Added DL of zip file

--- ShiftLeft.py
+++ ShiftLeft.py
@@ -2,19 +2,28 @@
 import urllib.request
 import sys
 import re
+import ast
+import zipfile
 
 def downloadWPPlugin (pluginName):
     if not re.match("^[a-z_-]*$", pluginName):
         print("Error! invalid chars in plugin name")
         sys.exit()
 
+
     pluginpage = "https://wordpress.org/plugins/" + pluginName + "/"
     response = urllib.request.urlopen(pluginpage)
     html = response.read()
     htmlstr = html.decode("utf-8")
-#    url=re.search("\"d(own)loadUrl\":\\s\"(.+?)\",",htmlstr,re.IGNORECASE)
     url=re.search('"downloadUrl":\\s"(.+?)",',htmlstr,re.IGNORECASE)
-    print(url.group(1))
+    dlurl = url.group(1).replace('\/','/')
+
+    #TODO: Probably file location, perms, randomised dl name etc.
+    finfo = urllib.request.urlretrieve(dlurl,'tmp/wp_tmp.zip')
+    with zipfile.ZipFile ('tmp/wp_tmp.zip') as zf:
+        zf.extractall('tmp')
+
+
 
     return
 
