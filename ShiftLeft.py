331881f867ee2cd7e3431329cc90813a0e5a2d4f
Glenn Pegden <glenn.pegden@skybettingandgaming.com>
2018-04-22 09:08:20 +0100

    Refactoring and added prettyness and SonarQube working much better

--- ShiftLeft.py
+++ ShiftLeft.py
@@ -16,10 +16,12 @@ import io
 from pprint import pprint
 from zapv2 import ZAPv2
 import config as cfg
+from termcolor import colored
 
+VERSION='0.1'
 
 def downloadWPPlugin (pluginName):
-    print("Downloading "+pluginName)
+    print("Checking plugin "+pluginName)
     if not re.match("^[a-z_-]*$", pluginName):
         print("Error! invalid chars in plugin name")
         sys.exit()
@@ -31,82 +33,92 @@ def downloadWPPlugin (pluginName):
     url=re.search('"downloadUrl":\\s"(.+?)",',htmlstr,re.IGNORECASE)
     dlurl = url.group(1).replace('\/','/')
 
-    #TODO: Probably file location, perms, randomised dl name etc.
-
     pathlib.Path(tmpFolder+'/dl').mkdir(parents=True, exist_ok=True) 
     pathlib.Path(tmpFolder+'/source').mkdir(parents=True, exist_ok=True) 
     pathlib.Path(tmpFolder+'/results').mkdir(parents=True, exist_ok=True) 
 
-    print("Downloading URL:" +dlurl)
+    print("Downloading " +dlurl)
     urllib.request.urlretrieve(dlurl,tmpFolder+'/dl/wp_tmp.zip')
 
-    print("unzipping")
+    print("Unzipping " +tmpFolder+'/dl/wp_tmp.zip')
     with zipfile.ZipFile (tmpFolder+'/dl/wp_tmp.zip') as zf:
         zf.extractall(tmpFolder+'/source/')
     return
 
 def scan_static_PHPCS():
 
-    print("Running static analysis using PHP Code Sniffer (with Security Rules)")
+    print("Running static analysis using "+colored('PHP Code Sniffer', 'red')+" [with Security Rules] from local install")
     for filename in os.listdir(tmpFolder+'/source/'):
         sourcefolder = tmpFolder+'/source/' + filename
         if os.path.isdir(tmpFolder+'/source/' + filename) == True:
-            print("Running php code sniffer on " + sourcefolder)
+#            print("Running php code sniffer on " + sourcefolder)
             subprocess.getoutput("~/ShiftLeft/tools/phpcs-security-audit/vendor/bin/phpcs --standard=~/ShiftLeft/tools/phpcs-security-audit/example_base_ruleset.xml " + sourcefolder + " > "+tmpFolder+"/results/phpcs.txt")  
 
+def scan_static_sonarqube():
+    print('Running static analysis using SonarQube (local client with dockerised scaning server) ', end='', flush=True)
+    cdir = os.getcwd()
+    os.chdir(tmpFolder+"/source")
+    cmd="/Users/gpe13/ShiftLeft/tools/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=WPTEST-PHP -Dsonar.sources=. -Dsonar.host.url=http://127.0.0.1:9000  -Dsonar.login=ad7d998a53d72ae326fac622294cfff0d8af1084 | tee "+tmpFolder+"/results/sonarQube.txt" 
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):          
+        print(".", end='', flush=True)
+#        print(line)
+    print("")
+    os.chdir(cdir)
+
 def scan_dynamic_burp():
-    print("Running dynamic analysis using Burp Pro (this can take a while!)")
+    print("Running dynamic analysis using Burp Pro via Proxy Server (this can take a while!)")
     cdir = os.getcwd()
     os.chdir(tmpFolder+"/results")
     subprocess.getoutput('java -jar -Xmx1024m -Djava.awt.headless=true  /Applications/Burp\ Suite\ Professional.app/Contents/java/app/burp/burpsuite_pro_1.7.33-18.jar http 127.0.0.1 8088 /')
     os.chdir(cdir)
 
 def scan_dynamic_zap():
-    print("Running dynamic analysis using OWASP Zap (this can take a while!)")
+    print("Running dynamic analysis using OWASP Zap via Proxy Server")
     zap = ZAPv2(apikey=cfg.ZAP_API)
     target="http://127.0.0.1:8088"
 
-    print('Accessing target {}'.format(target))
     zap.urlopen(target)
-    time.sleep(2)
 
-    print('Spidering target {}'.format(target))
+    print('-Zap Spider ', end='', flush=True)
     scanid = zap.spider.scan(target)
     time.sleep(2)
     while (int(zap.spider.status(scanid)) < 100):
-        print('Zap Spider progress %: {}'.format(zap.spider.status(scanid)))
+        print('.', end='', flush=True)
         time.sleep(2)
+    print('')
 
-    print ('Spider completed')
-
+    print('-Zap Passive Scan', end='', flush=True)
     while (int(zap.pscan.records_to_scan) > 0):
-        print ('Zap Records to passive scan : {}'.format(zap.pscan.records_to_scan))
+        print ('.', end='', flush=True)
         time.sleep(2)
+    print('')
 
-    print ('Passive Scan completed')
-
-    print ('Active Scanning target {}'.format(target))
+    print ('-Zap Active Scan', end='', flush=True)
     scanid = zap.ascan.scan(target)
     while (int(zap.ascan.status(scanid)) < 100):
-        print ('Zap Scan progress %: {}'.format(zap.ascan.status(scanid)))
+        print ('.', end='', flush=True)
         time.sleep(5)
+    print('')
 
-    print ('Active Scan completed')
     zaplog=open(tmpFolder+"/results/Zap.log","w")
     pprint(zap.core.alerts(),zaplog)
     zaplog.close()
 
 def scan_dynamic_nikto():
-    print("Running dynamic scan using nikto")
+    print("Running dynamic scan using nikto from local install", end='', flush=True)
     proc = subprocess.Popen("nikto -host 127.0.0.1 -port 8088 -ask no -Format htm -nointeractive -o "+tmpFolder+"/results/nikto.txt", shell=True, stdout=subprocess.PIPE)
-    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):  # or another encoding
-        print(line)
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):  
+        print(".", end='', flush=True)
+    print("")
 
 def scan_dynamic_wpscan():
-    print("Running dynamic scan using WPScan")
-    #There is probably a better option that using /private for this mapping
-    out=subprocess.getoutput("docker run -it  --net=shiftleft -v /private/"+tmpFolder+"/results/wpscan.txt:/wpscan/output.txt --rm wpscanteam/wpscan -u http://127.0.0.1:8088 --log /wpscan/output.txt")
-    print(out)
+    print("Running dynamic scan using WPScan from Docker Container", end='', flush=True)
+    cmd="docker run -it  --net=\"host\" -v /private/"+tmpFolder+"/results/:/tmp/wpscan/ --name my-wpscan-live --rm wpscanteam/wpscan -u http://host.docker.internal:8088 --follow-redirection --log /tmp/wpscan/wpscan.txt"
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"): 
+        print(".", end='', flush=True)
+    print("")
 
 def standupWordPress():
     print("Running WordPress Docker container")
@@ -118,7 +130,7 @@ def configureWP(pluginName):
     passwd=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
     out=subprocess.getoutput("docker-compose -f ~/ShiftLeft/wpdocker/docker-compose.yaml run --rm my-wpcli core install --url=http://127.0.0.1:8088 --title=Test --admin_user=admin --admin_password="+passwd+" --admin_email=test@test.com")
 #    print("SB1 "+out)
-    print("Configuring WordPress - Inistalling Plugin")
+    print("Configuring WordPress - Installing Plugin")
     out=subprocess.getoutput("docker-compose -f ~/ShiftLeft/wpdocker/docker-compose.yaml run --rm my-wpcli plugin install "+pluginName+" --activate")
     print("SB2 "+out)
     print("If all went well, WP should now be running on http://127.0.0.1:8088 the admin password is "+passwd)
@@ -131,19 +143,19 @@ def zipLogs(pluginName):
     print("Zipping up logs")
     shutil.make_archive(pluginName, "zip", tmpFolder+"/results")
 
+print(colored("ShiftLeft v"+VERSION),'green')
+
 if (len(sys.argv) > 1):
     pluginName = sys.argv[1]
 else:
     pluginName = "wordpress-seo"
 
 #Cleanup during testing, normally the closedown would run after all the tests had finished
-closedownWordPress()
+#closedownWordPress()
 
 
 tmpFolder=tempfile.mkdtemp()
-
 downloadWPPlugin(pluginName)
-scan_static_PHPCS()
 standupWordPress()
 
 for number in range(12):
@@ -160,10 +172,13 @@ for number in range(12):
 
 #TODO: Command line param to skip this
 print("Now is your time to go configure the plugin if it needs it. Just press a key to continue when you're ready to start scanning")
-input()
+#input()
 
-scan_dynamic_nikto()
+print("Let the scanning begin!")
+scan_static_PHPCS()
+scan_static_sonarqube()
 scan_dynamic_wpscan()
+scan_dynamic_nikto()
 scan_dynamic_burp()
 scan_dynamic_zap()
 
